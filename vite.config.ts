06be773753893730bae1b50865dd4603145839d2
emucs <80843974+EmuCS@users.noreply.github.com>
2026-05-30 18:36:13 +0200

    github pages test

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Fixes the base URL pathing for GitHub Pages
   base: '/complexvisualizer/',
-  
+  root: './', 
   build: {
-    // Tells Vite to compile index.html directly from the root folder
     outDir: 'dist',
-    assetsDir: 'assets',
-    rolldownOptions: {
-      external: ['node:module'],
-      output: {
-        globals: {
-          'node:module': 'Object',
-        }
-      }
-    }
   },
-  optimizeDeps: {
-    exclude: ['node:module']
-  }
 });
